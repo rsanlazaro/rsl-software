831c4a82846dde0ab76051f2ba22eb5d7ae82edb
rsanlazaro <rsanlazaro@hotmail.com>
2024-12-13 18:04:23 -0600

    version modified

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5c730b920da528e71b85654e5f0db5b273383846',
+        'name' => 'rsanl/frontend-store',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rsanl/frontend-store' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5c730b920da528e71b85654e5f0db5b273383846',
+            'dev_requirement' => false,
+        ),
+    ),
+);
